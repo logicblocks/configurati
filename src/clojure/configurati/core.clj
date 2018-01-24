30f00744238d057507799c385f9150d62a9c65a7
Toby Clemson <tobyclemson@gmail.com>
2018-01-24 01:14:53 +0000

    Add configuration specification.
    - Parameters can be nilable or not.
    - A specification can validate a configuration map.

--- src/clojure/configurati/core.clj
+++ src/clojure/configurati/core.clj
@@ -1,1 +1,51 @@
 (ns configurati.core)
+
+(def ^:private new-validation-result
+  {:missing []})
+
+(defn- missing? [parameter value]
+  (and
+    (nil? value)
+    (not (:nilable parameter))))
+
+(defn- mark-missing [validation-result name]
+  (update-in validation-result [:missing] #(conj % name)))
+
+(defn- determine-validation-result [parameters configuration-map]
+  (reduce
+    (fn [validation-result parameter]
+      (let [name (:name parameter)
+            value (name configuration-map)]
+        (cond
+          (missing? parameter value) (mark-missing validation-result name)
+          :else validation-result)))
+    new-validation-result
+    parameters))
+
+(defn- validation-result-empty? [validation-result]
+  (empty? (:missing validation-result)))
+
+(defprotocol ConfigurationValidator
+  (validate [configuration-specification configuration-map]))
+
+(defrecord ConfigurationSpecification [parameters]
+  ConfigurationValidator
+  (validate [this configuration-map]
+    (let [validation-result (determine-validation-result
+                              parameters
+                              configuration-map)
+          valid? (validation-result-empty? validation-result)]
+      (if valid?
+        configuration-map
+        (throw (ex-info
+                 "Configuration validation failed."
+                 validation-result))))))
+
+(defn configuration-specification [& parameters]
+  (->ConfigurationSpecification parameters))
+
+(defn with-parameter [name & rest]
+  (let [defaults {:nilable false}
+        base {:name name}
+        options (apply hash-map rest)]
+    (merge defaults base options)))
--- test/clojure/configurati/core_test.clj
+++ test/clojure/configurati/core_test.clj
@@ -1,7 +1,64 @@
 (ns configurati.core-test
   (:require
-    [clojure.test :refer :all]))
+    [clojure.test :refer :all]
 
-(deftest true-is-true
-  (testing "truth"
-      (is (= true true))))
+    [configurati.core :as c])
+  (:import [clojure.lang ExceptionInfo]))
+
+(defn validate-and-catch [specification configuration-map]
+  (try
+    (c/validate specification configuration-map)
+    (catch ExceptionInfo e e)))
+
+(deftest configuration-specification
+  (testing "validate"
+    (let [specification (c/configuration-specification
+                          (c/with-parameter :api-username
+                            :nilable false)
+                          (c/with-parameter :api-password
+                            :nilable false)
+                          (c/with-parameter :api-identifier
+                            :nilable true))]
+
+      (testing "returns map when no validation errors occur"
+        (let [configuration-map {:api-username   "some-username"
+                                 :api-password   "some-password"
+                                 :api-identifier nil}]
+          (is (= configuration-map
+                (c/validate specification configuration-map)))))
+
+      (testing "throws exception when non-nilable parameter is nil"
+        (let [configuration-map {:api-username   nil
+                                 :api-password   "some-password"
+                                 :api-identifier "some-identifier"}
+              exception (validate-and-catch
+                          specification configuration-map)]
+          (is (= ExceptionInfo (type exception)))
+          (is (= "Configuration validation failed."
+                (.getMessage exception)))
+          (is (= {:missing [:api-username]} (ex-data exception)))))
+
+      (testing (str "throws exception with all missing parameters when "
+                 "multiple non-nilable parameters are nil")
+        (let [configuration-map {:api-username   nil
+                                 :api-password   nil
+                                 :api-identifier nil}
+              exception (validate-and-catch
+                          specification configuration-map)]
+          (is (= ExceptionInfo (type exception)))
+          (is (= "Configuration validation failed."
+                (.getMessage exception)))
+          (is (= {:missing [:api-username
+                            :api-password]}
+                (ex-data exception))))))))
+
+(deftest with-parameter
+  (is (=
+        {:name :api-username :nilable false}
+        (c/with-parameter :api-username)))
+  (is (=
+        {:name :api-username :nilable false}
+        (c/with-parameter :api-username :nilable false)))
+  (is (=
+        {:name :api-username :nilable true}
+        (c/with-parameter :api-username :nilable true))))
