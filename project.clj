4915c272b9cef39d7e2793ec53323a5f9a2933c5
Toby Clemson <tobyclemson@gmail.com>
2018-01-27 19:31:28 +0000

    Prepare for 0.2.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject configurati "0.1.0"
+(defproject configurati "0.2.0-SNAPSHOT"
   :description "Define and resolve application configuration."
   :url "http://github.com/tobyclemson/configurati"
   :license {:name "The MIT License"
