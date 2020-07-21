fa7c9bfe995c988362ed3680dacd482479c7f190
Toby Clemson <tobyclemson@gmail.com>
2020-07-21 11:25:34 +0100

    Use latest build approach.

--- project.clj
+++ project.clj
@@ -1,31 +1,68 @@
-(defproject io.logicblocks/configurati "0.5.3-SNAPSHOT"
+(defproject io.logicblocks/configurati "0.5.4-RC1"
   :description "Define and resolve application configuration."
   :url "http://github.com/logicblocks/configurati"
 
   :license {:name "The MIT License"
             :url  "https://opensource.org/licenses/MIT"}
 
-  :plugins [[lein-cloverage "1.1.1"]
+  :plugins [[lein-cloverage "1.1.2"]
             [lein-shell "0.5.0"]
+            [lein-cprint "1.3.3"]
             [lein-ancient "0.6.15"]
             [lein-changelog "0.3.2"]
-            [lein-eftest "0.5.8"]
+            [lein-eftest "0.5.9"]
             [lein-codox "0.10.7"]
-            [lein-cljfmt "0.6.4"]
-            [lein-kibit "0.1.6"]
-            [lein-bikeshed "0.5.1"]]
+            [lein-cljfmt "0.6.7"]
+            [lein-kibit "0.1.8"]
+            [lein-bikeshed "0.5.2"]
+            [jonase/eastwood "0.3.11"]]
 
   :dependencies [[environ "1.1.0"]
                  [clj-yaml "0.4.0"]
                  [medley "1.0.0"]]
 
   :profiles
-  {:shared {:dependencies [[org.clojure/clojure "1.10.1"]
-                           [nrepl "0.6.0"]
-                           [eftest "0.5.8"]]}
-   :dev    [:shared {:source-paths ["dev"]
-                     :eftest {:multithread? false}}]
-   :test   [:shared {:eftest {:multithread? false}}]}
+  {:shared
+   ^{:pom-scope :test}
+   {:dependencies [[org.clojure/clojure "1.10.1"]
+                   [org.clojure/tools.trace "0.7.10"]
+
+                   [nrepl "0.7.0"]
+
+                   [eftest "0.5.9"]]}
+   :dev
+   [:shared {:source-paths ["dev"]
+             :eftest       {:multithread? false}}]
+   :test
+   [:shared {:eftest {:multithread? false}}]
+
+   :prerelease
+   {:release-tasks
+    [["shell" "git" "diff" "--exit-code"]
+     ["change" "version" "leiningen.release/bump-version" "rc"]
+     ["change" "version" "leiningen.release/bump-version" "release"]
+     ["vcs" "commit" "Pre-release version %s [skip ci]"]
+     ["vcs" "tag"]
+     ["deploy"]]}
+
+   :release
+   {:release-tasks
+    [["shell" "git" "diff" "--exit-code"]
+     ["change" "version" "leiningen.release/bump-version" "release"]
+     ["codox"]
+     ["changelog" "release"]
+     ["shell" "sed" "-E" "-i.bak" "s/\"[0-9]+\\.[0-9]+\\.[0-9]+\"/\"${:version}\"/g" "README.md"]
+     ["shell" "rm" "-f" "README.md.bak"]
+     ["shell" "git" "add" "."]
+     ["vcs" "commit" "Release version %s [skip ci]"]
+     ["vcs" "tag"]
+     ["deploy"]
+     ["change" "version" "leiningen.release/bump-version" "patch"]
+     ["change" "version" "leiningen.release/bump-version" "rc"]
+     ["change" "version" "leiningen.release/bump-version" "release"]
+     ["vcs" "commit" "Pre-release version %s [skip ci]"]
+     ["vcs" "tag"]
+     ["vcs" "push"]]}}
 
   :cloverage
   {:ns-exclude-regex [#"^user"]}
@@ -37,33 +74,12 @@
    :doc-paths   ["docs"]
    :source-uri  "https://github.com/logicblocks/configurati/blob/{version}/{filepath}#L{line}"}
 
-  :cljfmt {:indents ^:replace {#".*" [[:inner 0]]}}
+  :cljfmt {:indents {#".*"     [[:inner 0]]
+                     defrecord [[:block 1] [:inner 1]]
+                     deftype   [[:block 1] [:inner 1]]}}
+
+  :eastwood {:config-files ["config/linter.clj"]}
 
   :deploy-repositories
-  {"releases" {:url "https://repo.clojars.org" :creds :gpg}}
-
-  :release-tasks
-  [["shell" "git" "diff" "--exit-code"]
-   ["change" "version" "leiningen.release/bump-version" "release"]
-   ["codox"]
-   ["changelog" "release"]
-   ["shell" "sed" "-E" "-i" "" "s/\"[0-9]+\\.[0-9]+\\.[0-9]+\"/\"${:version}\"/g" "README.md"]
-   ["shell" "git" "add" "."]
-   ["vcs" "commit"]
-   ["vcs" "tag"]
-   ["deploy"]
-   ["change" "version" "leiningen.release/bump-version"]
-   ["vcs" "commit"]
-   ["vcs" "tag"]
-   ["vcs" "push"]]
-
-  :aliases {"test"      ["with-profile" "test" "eftest" ":all"]
-            "precommit" ["do"
-                         ["check"]
-                         ["kibit"]
-                         ["cljfmt" "fix"]
-                         ["bikeshed"
-                          "--name-collisions" "false"
-                          "--var-redefs" "false"
-                          "--verbose" "true"]
-                         ["test"]]})
+  {"releases"  {:url "https://repo.clojars.org" :creds :gpg}
+   "snapshots" {:url "https://repo.clojars.org" :creds :gpg}})
