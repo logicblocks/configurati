b22548d1e3fdb50bbaa1b9fef059f319ee3dfdb2
Circle CI <circleci@logicblocks.io>
2024-06-08 10:33:18 +0000

    Pre-release version 0.5.7-RC16 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/configurati "0.5.7-RC15"
+(defproject io.logicblocks/configurati "0.5.7-RC16"
   :description "Define and resolve application configuration."
   :url "http://github.com/logicblocks/configurati"
 
